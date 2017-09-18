d9eb955e9e9fc2d91a2baa3c705a137c47ce83cf
rogerbf <roger@bergfalk.nu>
2017-09-18 23:45:31 +0200

    rename variables

--- source/index.js
+++ source/index.js
@@ -2,8 +2,8 @@ export const min = limit => value => value >= limit ? value : limit
 
 export const max = limit => value => value <= limit ? value : limit
 
-export const constrain = ({ min: lower, max: upper }) => value => {
-  const lowerFn = min(lower)
-  const upperFn = max(upper)
-  return upperFn(lowerFn(value))
+export const constrain = ({ min: lowerLimit, max: upperLimit }) => value => {
+  const m = min(lowerLimit)
+  const M = max(upperLimit)
+  return M(m(value))
 }
